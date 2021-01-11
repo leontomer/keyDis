e19294080e14c7def4d2849b725909a633fc9a4c
Alex <alex.kreinis@walla.co.il>
2021-01-11 21:41:43 +0200

    done

--- server/routes/api/keys.js
+++ server/routes/api/keys.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var CryptoJS = require("crypto-js");
 
 const router = express.Router();
 
@@ -8,8 +9,6 @@ const key = new NodeRSA({ b: 1024 });
 var public_key = key.exportKey("public");
 var private_key = key.exportKey("private");
 router.get("/getkey", async (req, res) => {
-  console.log("entered");
-
   try {
     res.json({ key: public_key });
   } catch (err) {
@@ -19,14 +18,13 @@ router.get("/getkey", async (req, res) => {
 });
 
 router.post("/decrypt", async (req, res) => {
-  console.log("entered2");
   try {
-    const { textToDecrypt } = req.body;
-    console.log(textToDecrypt);
+    const { text, key } = req.body;
     const key_private = new NodeRSA(private_key);
-    console.log(
-      "decrypted message: " + key_private.decrypt(textToDecrypt, "utf8")
-    );
+    const AES_key = key_private.decrypt(key, "utf8");
+    var bytes = CryptoJS.AES.decrypt(text, AES_key);
+    var originalText = bytes.toString(CryptoJS.enc.Utf8);
+    console.log("original text is", originalText);
   } catch (err) {
     console.error(err.message);
     res.status(400).json("Error: " + err);
--- src/App.js
+++ src/App.js
@@ -1,33 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Chance from "chance";
+const chance = new Chance();
 const axios = require("axios");
 const NodeRSA = require("node-rsa");
+const CryptoJS = require("crypto-js");
 
 function App() {
-  const [text, setText] = useState("");
+  const [text, setText] = useState({ text: "", key: "" });
+  const [publicRSAkey, setPublicRSAkey] = useState();
 
-  const onSubmit = async () => {
-    const publicKey = await axios.get("/api/keys/getkey");
-    let key_public = new NodeRSA(publicKey.data.key);
-    const encriptedText = key_public.encrypt(text, "base64");
-    console.log(encriptedText);
-    await axios.post("/api/keys/decrypt", {
-      textToDecrypt: encriptedText,
-    });
+  useEffect(() => {
+    (async function () {
+      try {
+        const publicKey = await axios.get("api/keys/getkey");
+        setPublicRSAkey(publicKey.data.key);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+  const onSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      await axios.post("/api/keys/decrypt", text);
+    } catch (error) {
+      console.error("error is", error);
+    }
+  };
+
+  const handleOnCHange = (e) => {
+    const key = chance.guid();
+    var ciphertext = CryptoJS.AES.encrypt(e.target.value, key).toString();
+    let publicEncryption = new NodeRSA(publicRSAkey);
+    const encriptedKey = publicEncryption.encrypt(key, "base64");
+    const encriptBox = { text: ciphertext, key: encriptedKey };
+    console.log(text);
+    setText({ ...encriptBox });
   };
 
   return (
-    <div className="App">
-      <form onSubmit={onSubmit}>
-        <label>
-          text:
-          <input
-            type="text"
-            name="name"
-            onChange={(text) => setText(text.target.value)}
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+    <div
+      className="App"
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div>
+        <form onSubmit={onSubmit}>
+          <input type="text" name="name" onChange={handleOnCHange} />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     </div>
   );
 }
